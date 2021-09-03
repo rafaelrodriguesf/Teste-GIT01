e733fb205abd1c9092d3827f83f6521c88e6db1f
Rafael Rodrigues <rafalerf.18@gmail.com>
2021-09-03 19:48:06 -0300

    Adcionando como voce esta

--- Teste-GIT01/Program.cs
+++ Teste-GIT01/Program.cs
@@ -7,6 +7,7 @@ namespace Teste_GIT01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("How are you? ");
         }
     }
 }
