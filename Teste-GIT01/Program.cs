2324c522c558a1008f044eacdad1ee191fb8023e
Rafael Rodrigues <rafalerf.18@gmail.com>
2021-09-03 19:43:06 -0300

    Ola mundo

---
+++ Teste-GIT01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste_GIT01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
